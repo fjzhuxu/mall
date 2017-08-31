f0eb68cc4b8bb730951d8b4433d069d3b9d1bc4c
fjzhuxu <fjzhuxu@126.com>
2017-08-31 19:52:20 +0800

    2017年8月31日更新代码

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-   
-})
+  // template: '<App/>',
+  // components: { App }
+  render: h => h(App)
+});
+
+//.$mount("#app")

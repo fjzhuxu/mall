a1c531c81b14fb4b5422672c93a6ab799202d76d
fjzhuxu <fjzhuxu@126.com>
2017-09-19 11:35:45 +0800

    细节修改

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,14 @@ import OrderSuccess from '@/views/OrderSuccess'
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history',
   routes: [
     {
       path: '/',
       name: 'GoodsList',
       component: GoodsList
     },
-   {
+    {
       path: '/goods',
       name: 'GoodsList',
       component: GoodsList
@@ -39,7 +40,7 @@ export default new Router({
     {
       path: '/orderSuccess',
       name: 'OrderSuccess',
-      component:OrderSuccess
+      component: OrderSuccess
     }
   ]
 })
